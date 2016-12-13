c66e40ebf753f917ee0eae7a620acb2fa6d3238b
daweihuangstuy <dhuang9@stuy.edu>
2016-12-13 10:00:26 -0500

    updated HW#45

--- ALTester.java
+++ ALTester.java
@@ -1,15 +1,17 @@
-import java.util.ArrayList
+import java.util.ArrayList;
 
 public class ALTester {
-    public Object arr;
-    public boolean testSorted(Object arr){
+    public static void main(String[] args){
+	
+    }
+    public boolean testSorted(Object[] arr){
 	Object test;
 	test = arr[0];
 	for(int i = 1; i < arr.length; i++){
-	    if (arr[i] != (test + 1)){
+	    if (arr[i] != ((int)test + 1)){
 		return false;
-		test = arr[i];
 	    } 
+	    test = arr[i];
 	}
 	return true;
     }
