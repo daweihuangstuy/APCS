bdab4b32c15ab84ce4d2d33ad5a8d8c074a32582
daweihuangstuy <dhuang9@stuy.edu>
2016-12-15 10:00:21 -0500

    started HW#47

---
+++ GuessNumber.java
@@ -0,0 +1,27 @@
+//Dawei Huang
+//APCS1 pd4
+//HW46 -- ascending 
+//12/15/16
+
+import cs1.keyboard;
+import java.util.ArrayList;
+
+public class GuessNumber{
+    private ArrayList<Integer> _data;
+    private int targetInt = (int)(math.random() * 100 + 1);
+
+    public static void main(String[] args){
+	System.out.println("Welcome to the Guess-a-Number Game\n");
+	System.out.println("A random integer between 1 and 100 inclusive have been generated.")
+    }
+
+    public void genRandomNumber()
+    public GuessNumber(){
+	_data = newArrayList<Integer>();
+    }
+    public String toString(){
+	for(int i 
+    }
+
+
+}
