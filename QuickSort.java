8f68c70c391b44dd243b4be0f130765d1ab185c7
davidpwn360 <dhuang9@stuy.edu>
2017-03-13 23:56:32 -0400

    completed HW #15

--- QuickSort.java
+++ QuickSort.java
@@ -6,49 +6,49 @@
 /*****************************************************
  * class QuickSort
  * (skeleton) <<delete this line if untrue>>
- * Implements quicksort algo to sort an array of ints in place
+ * Implements quicksort algo to sort an arrayay of ints in place
  *
  * 1. Summary of QuickSort algorithm:
- * The QuickSort algorithm implements a divide and conquer methodology. Theoretically (best case), the 
- pivot selected end up around the middle of the array. Next, QuickSort is called on the two new segments,
- and so on, until the pivot is the same as the right bound and the left bound, at whih point the recursion is finished,
- as every position has only greater values to the right, and only lower values to the left. The worst case for the algorithm 
- however, is n^2. The BEST POSSIBLE is nlogn, meaning usually it is more.
- (I like mergesort more)
- * 2a. Worst pivot choice / array state and associated runtime: 
- * The worst pivot choice would be a pivot that is the smalles or largest of the inspected section. 
- This would only eliminate sort one number at a time. O(n^2)
- * 2b. Best pivot choice / array state and associated runtime:
- * The pivot whos final resting spot is directly in the middle of the inspected section. 
- This minimizaes the number of passes. O(nlogn)
- * 3. Approach to handling duplicate values in array:
- * Nothing. The code works with duplicates. :)
+	The QuickSort algorithm uses the divide and conquer method. The QuickSort algorithm divides the array into 
+	seperate segments and then QuickSort would be used on every index of the segments until it reaches the
+	rightmost index. At the point of the right most index, this recursion method is completed. I postulate that
+	the best possible runtime for this algorithm would be nlogn, the worst possible runtime would be n^2.
+ 
+ * 2a. Worst pivotPnt choice / arrayay state and associated runtime: 
+	The worst pivotPnt choice would be a pivot point that is either be the smallest or largest value in the
+	array segment.
+	
+ * 2b. Best pivotPnt choice / arrayay state and associated runtime:
+	The best pivotPnt choice would be a point that is directly at the center of the array segment.
+ * 3. Approach to handling duplicate values in arrayay:
+	This code appears to work well with duplicates because duplicates need not be organized into camps
+	that are either smaller or larger than the pivot position.
  *****************************************************/
 
 /***
     PROTIP: Assume no duplicates during initial development phase.
-    Once you have a working implementation, test against arrays 
+    Once you have a working implementation, test against arrayays 
     with duplicate values, and revise if necessary. (Backup first.)
  ***/
 
 public class QuickSort 
 {
     //--------------v  HELPER METHODS  v--------------
-    //swap values at indices x, y in array o
+    //swap values at indices x, y in arrayay o
     public static void swap( int x, int y, int[] o ) {
 	int tmp = o[x];
 	o[x] = o[y];
 	o[y] = tmp;
     }
 
-    //print input array 
-    public static void printArr( int[] a ) {
+    //print input arrayay 
+    public static void printarray( int[] a ) {
 	for ( int o : a )
 	    System.out.print( o + " " );
 	System.out.println();
     }
 
-    //shuffle elements of input array
+    //shuffle elements of input arrayay
     public static void shuffle( int[] d ) {
 	int tmp;
 	int swapPos;
@@ -59,12 +59,12 @@ public class QuickSort
 	}
     }
 
-    //return int array of size s, with each element fr range [0,maxVal)
-    public static int[] buildArray( int s, int maxVal ) {
-	int[] retArr = new int[s];
-	for( int i = 0; i < retArr.length; i++ )
-	    retArr[i] = (int)( maxVal * Math.random() );
-	return retArr;
+    //return int arrayay of size s, with each element fr range [0,maxVal)
+    public static int[] buildarrayay( int s, int maxVal ) {
+	int[] retarray = new int[s];
+	for( int i = 0; i < retarray.length; i++ )
+	    retarray[i] = (int)( maxVal * Math.random() );
+	return retarray;
     }
     //--------------^  HELPER METHODS  ^--------------
 
@@ -72,105 +72,101 @@ public class QuickSort
 
     /*****************************************************
      * void qsort(int[])
-     * @param d -- array of ints to be sorted in place
+     * @param d -- arrayay of ints to be sorted in place
      *****************************************************/
-    public static void qsort( int[] arr){
-	qsort(arr, 0, arr.length-1);
+    public static void qsort( int[] array){
+	qsort(array, 0, array.length-1);
     }
-    public static void qsort( int[] arr, int left, int right){
-	int pivot = partition(arr,left,right);
-	if (left < pivot - 1)
-	    qsort(arr,left,pivot - 1);
-	if (pivot + 1 < right)
-	    qsort(arr,pivot + 1 , right);
+    public static void qsort( int[] array, int leftSide, int rightSide){
+	int pivotPnt = partition(array,leftSide,rightSide);
+	if (leftSide < pivotPnt - 1)
+	    qsort(array,leftSide,pivotPnt - 1);
+	if (pivotPnt + 1 < rightSide)
+	    qsort(array,pivotPnt + 1 , rightSide);
 	return;
     }
 
-    public static int partition( int[] arr, int left , int right){
-	int pivVal = arr[left];
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        int pivFinalpos = left;
-        for (int i=left; i<right; i++){
-            if (arr[i]<pivVal){
-                int temp2 = arr[pivFinalpos];
-                arr[pivFinalpos] = arr[i];
-                arr[i] = temp2;
+    public static int partition( int[] array, int leftSide , int rightSide){
+	int pivVal = array[leftSide];
+        int temp = array[leftSide];
+        array[leftSide] = array[rightSide];
+        array[rightSide] = temp;
+        int pivFinalpos = leftSide;
+        for (int i=leftSide; i<rightSide; i++){
+            if (array[i]<pivVal){
+                int temp2 = array[pivFinalpos];
+                array[pivFinalpos] = array[i];
+                array[i] = temp2;
                 pivFinalpos+=1;
             }
         }
-        int temp3 = arr[pivFinalpos];
-        arr[pivFinalpos] = arr[right];
-        arr[right] = temp3;
+        int temp3 = array[pivFinalpos];
+        array[pivFinalpos] = array[rightSide];
+        array[rightSide] = temp3;
 	return pivFinalpos;//return any one
     }
-	
-
-    
-    
 
     //main method for testing
     public static void main( String[] args ) 
     {
 	
 
-	//get-it-up-and-running, static test case:
-	int [] arr1 = {7,1,5,12,3};
-	System.out.println("\narr1 init'd to: " );
-	printArr(arr1);
+	//main method for testing static cases
+	int [] array1 = {7,1,5,12,3};
+	System.out.println("\narray1 init'd to: " );
+	printarray(array1);
 
 	
-	qsort( arr1 , 0 , arr1.length-1);	
-       	System.out.println("arr1 after qsort: " );
-	printArr(arr1);
+	qsort( array1 , 0 , array1.length-1);	
+       	System.out.println("array1 after qsort: " );
+	printarray(array1);
 
-	// randomly-generated arrays of n distinct vals
-	int[] arrN = new int[10];
-	for( int i = 0; i < arrN.length; i++ )
-	    arrN[i] = i;
+	// randomly-generated arrayays of n distinct vals
+	int[] arrayN = new int[10];
+	for( int i = 0; i < arrayN.length; i++ )
+	    arrayN[i] = i;
        
-	System.out.println("\narrN init'd to: " );
-	printArr(arrN);
+	System.out.println("\narrayN init'd to: " );
+	printarray(arrayN);
 
-       	shuffle(arrN);
-       	System.out.println("arrN post-shuffle: " );
-	printArr(arrN);
+       	shuffle(arrayN);
+       	System.out.println("arrayN post-shuffle: " );
+	printarray(arrayN);
 
-	qsort( arrN , 0 , arrN.length-1);
-	System.out.println("arrN after sort: " );
-	printArr(arrN);
+	qsort( arrayN , 0 , arrayN.length-1);
+	System.out.println("arrayN after sort: " );
+	printarray(arrayN);
 	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
 	//~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
 
-	//get-it-up-and-running, static test case w/ dupes:
-	int [] arr2 = {7,1,5,12,3,7};
-	System.out.println("\narr2 init'd to: " );
-	printArr(arr2);
+	//duplicate cases included
+	int [] array2 = {7,1,5,12,3,7};
+	System.out.println("\narray2 init'd to: " );
+	printarray(array2);
 
-	qsort( arr2 );	
-       	System.out.println("arr2 after qsort: " );
-	printArr(arr2);
+	qsort( array2 );	
+       	System.out.println("array2 after qsort: " );
+	printarray(array2);
 
 
-	// arrays of randomly generated ints
-	int[] arrMatey = new int[20];
-	for( int i = 0; i < arrMatey.length; i++ )
-	    arrMatey[i] = (int)( 48 * Math.random() );
+	// randomly generated integers
+	int[] arrayTest = new int[20];
+	for( int i = 0; i < arrayTest.length; i++ )
+	    arrayTest[i] = (int)( 48 * Math.random() );
        
-	System.out.println("\narrMatey init'd to: " );
-	printArr(arrMatey);
+	System.out.println("\narrayTest init'd to: " );
+	printarray(arrayTest);
 
-       	shuffle(arrMatey);
-       	System.out.println("arrMatey post-shuffle: " );
-	printArr(arrMatey);
+       	shuffle(arrayTest);
+       	System.out.println("arrayTest post-shuffle: " );
+	printarray(arrayTest);
 
-	qsort( arrMatey );
-	System.out.println("arrMatey after sort: " );
-	printArr(arrMatey);
+	qsort( arrayTest );
+	System.out.println("arrayTest after sort: " );
+	printarray(arrayTest);
 //	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     }//end main
