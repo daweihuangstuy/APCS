eddd5e34412749b53525157f5f84e9fc5ff6a1f1
daweihuangstuy <dhuang9@stuy.edu>
2017-03-23 11:48:26 -0400

    added hw#22 file

---
+++ LatKtS.java
@@ -0,0 +1,57 @@
+/*****************************************************
+ * class LatKtS (LatKeysToSuccess)
+ * Driver class for Latkes.
+ * Uses a stack to reverse a text string, check for sets of matching parens.
+ * SKELETON
+ *****************************************************/
+
+
+public class LatKtS 
+{
+
+    /**********************************************************
+     * precondition:  input string has length > 0
+     * postcondition: returns reversed string s
+     *                flip("desserts") -> "stressed"
+     **********************************************************/
+    public static String flip( String s ) 
+    { 
+        Latkes stack = new Latkes(s.length - 1);
+	String retString = "";
+	for(int i = 0; i < s.length; i++){
+	    stack.push(String.subset(i, i + 1));
+	}
+        while(stack.size() != 0){
+	    retString = stack.pop();
+	}
+        return retString;
+    }//end flip()
+
+
+    /**********************************************************
+     * precondition:  s contains only the characters {,},(,),[,]
+     * postcondition: allMatched( "({}[()])" )    -> true
+     *                allMatched( "([)]" )        -> false
+     *                allMatched( "" )            -> true
+     **********************************************************/
+    public static boolean allMatched( String s ) 
+    { 
+
+    }//end allMatched()
+
+
+    //main method to test
+    public static void main( String[] args ) { 
+
+	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+	System.out.println(flip("stressed"));
+        System.out.println(allMatched( "({}[()])" )); //true
+        System.out.println(allMatched( "([)]" ) ); //false
+        System.out.println(allMatched( "(){([])}" ) ); //true
+        System.out.println(allMatched( "](){([])}" ) ); //false
+        System.out.println(allMatched( "(){([])}(" ) ); //false
+        System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
+          ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+    }
+
+}//end class LatKtS
