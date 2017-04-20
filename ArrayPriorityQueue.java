2ba532d0d40bab986dfae54c508ae0d5aefddc21
davidpwn360 <dhuang9@stuy.edu>
2017-04-20 02:05:52 -0400

    completed HW #32

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,23 +1,52 @@
-import java.utils.PriorityQueue
+// Dawei Huang
+// APCS2 pd5
+// HW32 -- Getting Past the Velvet Rope
+// 2017-04-2
 
-public class ArrayPriorityQueue implements PriorityQueue{
+import java.util.ArrayList;
+
+public class ArrayPriorityQueue implements PriorityQueue {
     private ArrayList<Integer> _data = new ArrayList<Integer>();
     public void add(int num){
-	_data.add(num);
+		_data.add(num);
     }
     public boolean isEmpty(){
-	return (_data.size() == 0);
+		return (_data.size() == 0);
     }
     public int peekMin(){
-	int minVal = _data.get(0);
-	for(int x : _data){
-	    if (x < minVal){
-		minVal = x;
-	    }
-	}
+		int minVal = _data.get(0);
+		for(int x : _data){
+			if (x < minVal){
+				minVal = x;
+			}
+		}
+		return minVal;
     }
     public int removeMin(){
-	int minVal 
+		int minVal = peekMin();
+		for(int i = 0; i < _data.size(); i++){
+			if(minVal == _data.get(i)){
+				_data.remove(i);
+				break;
+			}
+		}
+		return minVal;
     }
+	public String toString(){
+		return _data.toString();
+	}
+	
+	public static void main(String[] args){
+		ArrayPriorityQueue bob = new ArrayPriorityQueue();
+		for(int i = 0; i < 10; i++){
+			bob.add((int)(Math.random() * 100));
+		}
+		System.out.println(bob);
+		while(bob.isEmpty() == false){
+			System.out.println(bob.peekMin());
+			System.out.println(bob.removeMin());
+			System.out.println(bob);
+		}
+	}
 
 }
---
+++ PriorityQueue.java
@@ -0,0 +1,11 @@
+// Dawei Huang
+// APCS2 pd5
+// HW32 -- Getting Past the Velvet Rope
+// 2017-04-2
+
+interface PriorityQueue{
+	public void add(int x);
+	public boolean isEmpty();
+	public int peekMin();
+	public int removeMin();
+}
